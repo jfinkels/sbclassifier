7bc2cc4dae4414378da8865ef32234f1bb6029d4
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-09-17 01:05:49 -0400

    Adds requirements and setup information.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    author='Jeffrey Finkelstein',
+    author_email='jeffrey.finkelstein@gmail.com',
+    #classifiers=[],
+    #description='',
+    #download_url='',
+    install_requires=['lockfile'],
+    #include_package_data=True,
+    #keywords=[],
+    #license='',
+    #long_description='',
+    name='sbclassifier',
+    platforms='any',
+    #packages=[],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    url='http://github.com/jfinkels/sbclassifier',
+    version='0.0.1-dev',
+    #zip_safe=False
+)
